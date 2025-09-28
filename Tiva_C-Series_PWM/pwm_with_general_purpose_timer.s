565bbcbc56f2c232bbdcb36c5edc6e34752523cf
Thomas Strade <99762399+tstrade@users.noreply.github.com>
2025-09-27 21:00:37 -0400

    Blinky works, just need to nail down frequency. Adv. RGB works well as far as I can tell

---
+++ Tiva_C-Series_PWM/pwm_with_general_purpose_timer.s
@@ -0,0 +1,225 @@
+	.data
+
+start_prompt:	.string "RGB LED", 0
+
+rgb_prev_state:		.byte 0x00
+
+rgb_input:			.word 0x000000
+
+red_duty_cycle: 	.byte 0x00
+blue_duty_cycle:	.byte 0x00
+green_duty_cycle:	.byte 0x00
+
+reset_rgb:			.word 0x000000
+
+	.text
+
+	.global Blinky
+	.global Advanced_RGB_LED
+	.global illuminate_RGB_LED
+	.global rgb_led_init
+	.global Timer_Handler
+
+	.global uart_interrupt_init
+	.global timer_interrupt_init
+	.global uart_init
+	.global output_character
+	.global read_character
+	.global read_string
+	.global output_string
+
+	.global string2int
+	.global int2string
+	.global division
+	.global multiplication
+	.global newline
+
+ptr_to_start_prompt:	.word start_prompt
+ptr_to_rgb_prev_state:	.word rgb_prev_state
+ptr_to_rgb_input:		.word rgb_input
+ptr_to_red_dc:			.word red_duty_cycle
+ptr_to_blue_dc:			.word blue_duty_cycle
+ptr_to_green_dc:		.word green_duty_cycle
+ptr_to_reset_rgb:		.word reset_rgb
+
+
+; General Purpose Timers
+GPTMCFG:          .equ 0x000	; GPTM Configuration
+GPTMTAMR:         .equ 0x004	; GPTM Timer A Mode
+GPTMCTL:          .equ 0x00C	; GPTM Control
+GPTMIMR:          .equ 0x018	; GPTM Interrupt Mask
+GPTMICR:	      .equ 0x024	; GPTM Interrupt Clear Register
+GPTMTAILR:        .equ 0x028	; GPTM Timer A Interval Load
+GPTMTAR:          .equ 0x048	; GPTM Timer A
+
+; General Purpose Input / Output
+GPIODATA_A:       .equ 0x3F0	; GPIO Data Register Port A
+GPIODATA_D:		  .equ 0x03C	; GPIO Data Register Port D
+GPIODIR:          .equ 0x400	; GPIO Direction Register
+
+GPIOIS:		      .equ 0x404	; GPIO Interrupt Sense
+GPIOIBE:	      .equ 0x408	; GPIO Interrupt Both Edges
+GPIOIEV:	      .equ 0x40C	; GPIO Interrupt Event
+GPIOIM:		      .equ 0x410	; GPIO Interrupt Mask
+GPIORIS:		  .equ 0x414	; GPIO Raw Interrupt Status
+GPIOICR:	      .equ 0x41C	; GPIO Interrupt Clear Register
+
+GPIOAFSEL:		  .equ 0x420	; GPIO Alternate Function Select
+GPIOPUR:          .equ 0x510	; GPIO Pull-Up Resistor Register
+GPIOPDR:		  .equ 0x514	; GPIO Pull-Down Resistor Register
+GPIODEN:          .equ 0x51C	; GPIO Digital Register
+GPIOCTL:		  .equ 0x52C	; GPIO Port Control
+GPIORCGC:         .equ 0x608   	; GPIO Run Mode Clock Gating Control
+
+; Universal Asynchronous Receiver/Transmitters
+UARTDR:			  .equ 0x000	; UART Data Register
+U0FR: 			  .equ 0x018	; UART0 Flag Register
+UARTIBRD: 		  .equ 0x024	; UART Integer Baud-Rate Divisor
+UARTFBRD: 		  .equ 0x028	; UART Fractional Buad-Rate Divisor
+UARTLCRH:		  .equ 0x02C	; UART Line Control
+UARTCTL:		  .equ 0x030	; UART Control
+UARTIM:           .equ 0x038	; UART Interrupt Mask
+UARTICR:          .equ 0x044	; UART Interrupt Clear Register
+UARTRCGC:		  .equ 0x618	; UART Run Mode Clock Gating Control
+UARTCC:			  .equ 0xFC8	; UART Clock Configuration
+
+RXIM:             .equ 0x010	; UART Receive Interrupt Mask
+RXIC:             .equ 0x010	; UART Receive Raw Interrupt Status
+
+
+RCGCTIMER:        .equ 0x604	; 16/32-Bit General-Purpose Timer Run Mode Clock Gating Control
+
+; Cortex M4 Peripherals
+ENUART0:          .equ 0x020	; UART0 Enable Bit
+ENGPIOD:		  .equ 0x008	; GPIO Port D Enable Bit
+EN0:              .equ 0x100	; Interrupt 0-31 Set Enable
+
+; Misc.
+NULL:			   .equ 0x000
+
+; Program-specific
+GPIO_PORTF_RCGC:  .equ 0x020	; GPIO Port F Enable RCGC Mask
+RGB_LED_PINS:  	  .equ 0x00E	; GPIO Port F Pins 1-3 Mask
+RED_LED:		  .equ 0x002	; PF1 (Red) Mask
+BLUE_LED:		  .equ 0x004	; PF2 (Blue) Mask
+GREEN_LED:		  .equ 0x008	; PF3 (Green) Mask
+
+rgb_led_init:
+	PUSH {lr}
+	; PF1 = RED
+	; PF2 = BLUE
+	; PF3 = GREEN
+
+	; System control base address
+	MOV r0, #0xE000
+	MOVT r0, #0x400F
+
+	; Set GPIORCGC[5] to enable clock
+	;	for GPIO Port F
+	LDR r1, [r0, #GPIORCGC]
+	ORR r1, r1, #GPIO_PORTF_RCGC
+	STR r1, [r0, #GPIORCGC]
+	nop
+
+	; GPIO Port F base address
+	MOV r0, #0x5000
+	MOVT r0, #0x4002
+
+	; Set PF1-3 as output
+	LDR r1, [r0, #GPIODIR]
+	ORR r1, r1, #RGB_LED_PINS
+	STR r1, [r0, #GPIODIR]
+
+	; Set PF1-3 Digital Enable
+	LDR r1, [r0, #GPIODEN]
+	ORR r1, r1, #RGB_LED_PINS
+	STR r1, [r0, #GPIODEN]
+
+	POP {pc}
+
+
+
+
+
+
+Blinky:
+	PUSH {lr}
+
+
+
+	POP {pc}
+
+
+
+Advanced_RGB_LED:
+	PUSH {lr}
+
+	BL rgb_led_init
+
+	LDR r0, ptr_to_rgb_input
+	MOV r1, #0xFF2E
+	MOVT r1, #0x0070
+	STR r1, [r0]
+
+	LDR r0, ptr_to_reset_rgb
+	STR r1, [r0]
+	BL timer_interrupt_init
+
+loop_forever:
+	B loop_forever
+
+
+	POP {pc}
+
+
+
+Timer_Handler:
+	PUSH {lr}
+
+	; Timer 0 base address
+	MOV r0, #0x0000
+	MOVT r0, #0x4003
+
+	; Set GPTMICR[0] to clear interrupt
+	LDR r1, [r0, #GPTMICR]
+	ORR r1, r1, #0x001
+	STR r1, [r0, #GPTMICR]
+
+	; r0 = input to illuminate_RGB_LED
+	; r1 =
+	MOV r0, #NULL
+	MOV r1, #0x0101
+	MOVT r1, #0x0001
+
+	LDR r3, ptr_to_rgb_input
+	LDR r2, [r3]
+	TST r2, #0xFF0000
+	ITT NE
+	SUBNE r2, r2, #0x010000
+	ORRNE r0, r0, #RED_LED
+
+	TST r2, #0x00FF00
+	ITT NE
+	SUBNE r2, r2, #0x000100
+	ORRNE r0, r0, #GREEN_LED
+
+	TST r2, #0x0000FF
+	ITT NE
+	SUBNE r2, r2, #0x000001
+	ORRNE r0, r0, #BLUE_LED
+
+	CMP r2, #NULL
+	ITTT EQ
+	LDREQ r1, ptr_to_reset_rgb
+	LDREQ r1, [r1]
+	MOVEQ r2, r1
+
+	STR r2, [r3]
+
+	BL illuminate_RGB_LED
+
+	POP {lr}
+	BX lr
+
+
+	.end
