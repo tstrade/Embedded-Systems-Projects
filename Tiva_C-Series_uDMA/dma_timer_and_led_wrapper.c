6cdc023f2d4ebc7546df547bf7217ba108f46601
Thomas Strade <99762399+tstrade@users.noreply.github.com>
2025-10-15 13:49:23 -0400

    init uart to allow control when testing
    Refactor initialization functions and enable UART.

--- Tiva_C-Series_uDMA/dma_timer_and_led_wrapper.c
+++ Tiva_C-Series_uDMA/dma_timer_and_led_wrapper.c
@@ -5,70 +5,83 @@
 static void dma_init ( void );
 static void timer_init ( void );
 static void gpio_init ( void );
-extern uint32_t ptr_to_channel_control_table;
+static void uart_init ( void );
+
+extern uint32_t ptr_to_channel_control;
+extern uint32_t read_character ( void );
+
+#define ASCII_ESC (0x01B)
 /**
  * main.c
  */
 int main(void)
 {
-    //dma_init ();
-    //timer_init ();
     gpio_init ();
-
-    int i = 15;
-    while (i--)
-    {
-    *(uint32_t *)(GPIO_PORTBP_BASE_ADDR + (uint32_t)0x3FC) += 0x01;
-    }
-
-    timer_init ();
+    uart_init ();
     dma_init ();
+    timer_init ();
+
+    while (read_character () != ASCII_ESC);
 
     return 0;
 }
 
+
+
+
+static void
+timer_init ( void )
+{
+    *(uint32_t *)(SYS_CONTROL_BASE_ADDR + RCGCTIMER)  |= 0x1;      // Enable system clock for GPTM
+    asm (" NOP");
+    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMCTL)     = 0x0;      // Disable Timer 0A
+    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMCFG)     = 0x0;      // Select 32-bit configuration
+    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMTAMR)   |= 0x2;      // Set Timer 0 for periodic mode
+    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMTAILR)   = 0xF42400; // Trigger event every 1s (16e6 cycles)
+    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMIMR)    |= 0x1;      // Enable Timer 0A time-out interrupts
+    *(uint32_t *)(CM4_PERIPHS_BASE_ADDR + EN0)        |= 0x80000U; // Enable interrupts on vector table
+    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMCTL)    |= 0x1;      // Enable Timer 0A
+}
+
+static void
+gpio_init ( void )
+{
+    *(uint32_t *)(SYS_CONTROL_BASE_ADDR + RCGCGPIO)     |= 0x2;     // Enable system clock for GPIO Port B
+    asm (" NOP");
+    *(uint32_t *)(GPIO_PORTBP_BASE_ADDR + GPIODIR)      |= 0x0F;    // Set PB0-3 as output
+    *(uint32_t *)(GPIO_PORTBP_BASE_ADDR + GPIOAFSEL)    &= (~0x0F); // Use as GPIO pins
+    *(uint32_t *)(GPIO_PORTBP_BASE_ADDR + GPIOPDR)      |= 0x0F;    // Use pull-down resistor
+    *(uint32_t *)(GPIO_PORTBP_BASE_ADDR + GPIODEN)      |= 0x0F;    // Set digital enable
+}
+
+static void
+uart_init ( void )
+{
+    *(uint32_t *)(SYS_CONTROL_BASE_ADDR + RCGCUART)     |= 0x1;   // Enable system clock for UART0
+    asm (" NOP");
+    *(uint32_t *)(UART_MODUL0_BASE_ADDR + UARTCTL)       = 0x0;   // Disable UART0 Control
+    *(uint32_t *)(UART_MODUL0_BASE_ADDR + UARTIBRD)      = 0x8;   // Set for 115200 baud
+    *(uint32_t *)(UART_MODUL0_BASE_ADDR + UARTFBRD)      = 0x2C;  // Set for 115200 baud
+    *(uint32_t *)(UART_MODUL0_BASE_ADDR + UARTCC)        = 0x0;   // Use system clock
+    *(uint32_t *)(GPIO_PORTAP_BASE_ADDR + GPIODEN)      |= 0x3;   // Make PA0-1 digital ports
+    *(uint32_t *)(GPIO_PORTAP_BASE_ADDR + GPIOAFSEL)    |= 0x3;   // Set PA0-1 as alt. function
+    *(uint32_t *)(GPIO_PORTAP_BASE_ADDR + GPIOPCTL)     |= 0x11;  // Configure PA0-1 for UART
+}
 
 static void
 dma_init ( void )
 {
     uint32_t CHANNELS = 0x00040020; // Channels 5 & 18 correspond to Timer 0A & GPIO Port B
 
-    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + RCGCDMA)         = 0x1;      // Enable system clock for DMA
+    *(uint32_t *)(SYS_CONTROL_BASE_ADDR + RCGCDMA)         = 0x1;      // Enable system clock for DMA
     asm (" NOP");
     *(uint32_t *)(UDIR_MEMACC_BASE_ADDR + DMACFG)          = 0x1;      // Enable uDMA controller
-    *(uint32_t *)(UDIR_MEMACC_BASE_ADDR + DMACTLBASE)      = ptr_to_channel_control_table;
+    *(uint32_t *)(UDIR_MEMACC_BASE_ADDR + DMACTLBASE)      = ptr_to_channel_control;
     *(uint32_t *)(UDIR_MEMACC_BASE_ADDR + DMAPRIOSET)      = CHANNELS; // Select default priority
     *(uint32_t *)(UDIR_MEMACC_BASE_ADDR + DMAALTCLR)       = CHANNELS; // Select for primary control structure
     *(uint32_t *)(UDIR_MEMACC_BASE_ADDR + DMAUSEBURSTSET)  = CHANNELS; // Set burst requests only
     *(uint32_t *)(UDIR_MEMACC_BASE_ADDR + DMAREQMASKCLR)   = CHANNELS; // Allow uDMA controller to recognize requests
 }
-
-static void
-timer_init ( void )
-{
-    *(uint32_t *)(SYS_CONTROL_BASE_ADDR + RCGCTIMER)   = 0x1;      // Enable system clock for GPTM
-    asm (" NOP");
-    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMCTL)     = 0x0;      // Disable Timer 0A
-    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMCFG)     = 0x0;      // Select 32-bit configuration
-    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMTAMR)    = 0x2;      // Set Timer 0 for periodic mode
-    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMTAILR)   = 0x7A1200; // Trigger event every 0.5s (8e6 cycles)
-    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMIMR)     = 0x1;      // Enable Timer 0A time-out interrupts
-    *(uint32_t *)(GPTM_TIMER0_BASE_ADDR + GPTMCTL)     = 0x1;      // Enable Timer 0A
-    *(uint32_t *)(CM4_PERIPHS_BASE_ADDR + EN0)         = 0x80000U; // Enable interrupts on vector table
-}
-
-static void
-gpio_init ( void )
-{
-    *(uint32_t *)(SYS_CONTROL_BASE_ADDR + RCGCGPIO)     |= 0x2;  // Enable system clock for GPIO Port B
-    asm (" NOP");
-    *(uint32_t *)(GPIO_PORTBP_BASE_ADDR + GPIODIR)      |= 0x0F;  // Set PB0-3 as output
-    *(uint32_t *)(GPIO_PORTBP_BASE_ADDR + GPIOAFSEL)    &= (~0x0F);
-    *(uint32_t *)(GPIO_PORTBP_BASE_ADDR + GPIOPDR)      |= 0x0F;
-    *(uint32_t *)(GPIO_PORTBP_BASE_ADDR + GPIODEN)      |= 0x0F;  // Set digital enable
-    // GPIOIS, GPIOIBE, GPIOEV, GPIOIM
-}
-
 
 /*
     ; LEDs on Port B, Pins 0,1,2,3
@@ -143,7 +156,7 @@ gpio_init ( void )
     ;
     ;   DSTINC      (31:30) = 0b11 -> No dest. addr increment
     ;   DSTSIZE     (28:28) = 0b00 -> 8-bit dest. data size
-    ;   SRCINC      (27:26) = 0b11 -> No src. addr increment
+    ;   SRCINC      (27:26) = 0b00 -> 8-bit src. addr increment
     ;   SRCSIZE     (25:24) = 0b00 -> 8-bit src. data size
     ;   Reserved    (23:18)
     ;   ARBSIZE     (17:14) = 0x00 -> Arbitrates after 1 transfer
@@ -151,7 +164,7 @@ gpio_init ( void )
     ;   NXTUSEBURST (3)     = 0b01 -> Use burst mode
     ;   XFERMODE    (2:0)   = 0b01 -> Use basic transfer mode
     ;
-    ;   >>> Control Word = 0xCC000009
+    ;   >>> Control Word = 0xC0000009
     ;
     ;       ! Must be reconfigured between each transfer !
     ;       ! (src/dest pointers will not change)        !
