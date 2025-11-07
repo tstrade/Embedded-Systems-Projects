7cfb899d3626a4fe6594b1eda4439475b57a3221
Thomas Strade <99762399+tstrade@users.noreply.github.com>
2025-11-07 09:50:02 -0500

    Add Timer0 interrupt handlers and MOVF macros
    Implement Timer0 interrupt handlers and macros for loading values.

---
+++ Tiva_C-Series_SPI/spi.s
@@ -0,0 +1,47 @@
+	.data
+
+	.text
+
+	.global Timer0_Handler
+	.global Timer1_Handler
+
+	.sect "macros"
+
+; Move Full
+;       Takes a 32-bit immediate and loads that value into rd
+MOVF 	.macro reg, data
+		MOV reg, #(data & 0x0000FFFF)
+		MOVT reg, #((data & 0xFFFF0000) >> 16)
+		.endm
+
+
+; Move Full NOT
+;       Takes a 32-bit immediate and loads NOT(value) into rd
+MOVFN 	.macro rd, immed
+		MOVF rd, (~immed)
+		.endm
+
+	.text
+
+Timer0A_Handler:
+	PUSH {lr}
+
+	MOVF r0, 0x40030024
+	LDR r1, [r0]
+	ORR r1, r1, #0x1
+	STR r1, [r0]
+
+	POP {lr}
+	BX lr
+
+
+Timer0B_Handler:
+	PUSH {lr}
+
+	MOVF r0, 0x40030024
+	LDR r1, [r0]
+	ORR r1, r1, #0x100
+	STR r1, [r0]
+
+	POP {lr}
+	BX lr
