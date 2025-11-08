47453fc1ebd2913d005bc975ccdf64e0ea04ee86
Thomas Strade <99762399+tstrade@users.noreply.github.com>
2025-11-07 20:06:31 -0500

    Enhance SPI handler with buffer and display pointers
    Added buffer and display pointers for SPI data handling.

--- Tiva_C-Series_SPI/spi.s
+++ Tiva_C-Series_SPI/spi.s
@@ -1,9 +1,18 @@
 	.data
 
+buffer:		.word 0x00000000
+			.word 0x00000000
+
+display:	.word 0x00000000
+
 	.text
 
 	.global Timer0A_Handler
-	.global Timer0B_Handler
+	.global ptr_to_buffer
+	.global ptr_to_display
+
+ptr_to_buffer:		.word buffer
+ptr_to_display: 	.word display
 
 	.sect "macros"
 
@@ -26,21 +35,54 @@ MOVFN 	.macro rd, immed
 Timer0A_Handler:
 	PUSH {lr}
 
+	; Clear interrupt status
 	MOVF r0, 0x40030024
 	LDR r1, [r0]
 	ORR r1, r1, #0x1
 	STR r1, [r0]
 
-	POP {lr}
-	BX lr
+	; Unlatch
+	MOVF r0, 0x400063FC
+	MOV r1, #0x0
+	STR r1, [r0]
 
-
-Timer0B_Handler:
-	PUSH {lr}
-
-	MOVF r0, 0x40030024
+	; Check if transmitting
+	MOVF r0, 0x4000A00C
+wait_on_prev:
 	LDR r1, [r0]
-	ORR r1, r1, #0x100
+	AND r1, r1, #0x10
+	CMP r1, #0x10
+	BEQ wait_on_prev
+
+
+	; Write data once free
+	MOVF r0, 0x4000A008
+
+	LDR r1, ptr_to_display
+	LDR r2, [r1]
+	LDRH r3, [r2], #0x2
+	STRH r3, [r0]
+
+	LDR r3, ptr_to_buffer
+	ADD r3, r3, #0x8
+
+	CMP r2, r3
+	ITTE EQ
+	SUBEQ r3, r3, #0x8
+	STREQ r3, [r1]
+	STRNE r2, [r1]
+
+	; Check if transmitting
+	MOVF r0, 0x4000A00C
+wait_on_curr:
+	LDR r1, [r0]
+	AND r1, r1, #0x10
+	CMP r1, #0x10
+	BEQ wait_on_curr
+
+	; Latch
+	MOVF r0, 0x400063FC
+	MOV r1, #0x80
 	STR r1, [r0]
 
 	POP {lr}
