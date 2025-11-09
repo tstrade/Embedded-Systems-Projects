a37ea281aecaa8729a41256a344c5d1f21024450
Thomas Strade <99762399+tstrade@users.noreply.github.com>
2025-11-09 12:17:32 -0500

    Refactor ADC initialization and sampling loop

--- Tiva_C-Series_ADC/adc_wrapper.c
+++ Tiva_C-Series_ADC/adc_wrapper.c
@@ -1,4 +1,5 @@
 #include "tm4c123gh6pm_registers.h"
+#include <stdio.h>
 
 static void adc_init ( void );
 
@@ -8,29 +9,32 @@ static void adc_init ( void );
 int main(void)
 {
     adc_init ();
-    while (1);
+
+    uint32_t value;
+
+    while (1)
+    {
+        *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCPSSI) = 0x8;
+        while ( (*(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCRIS) & 0x8) == 0x0);
+        value = *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCSSFIFO3) & 0x0FFF;
+        *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCISC) = 0x8;
+    }
 	return 0;
 }
 
 static void
 adc_init ( void )
 {
-    *(uint32_t *)(SYS_CONTROL_BASE_ADDR + RCGCADC)      = 0x3;      // Provide system clock to both modules for now
+    *(uint32_t *)(SYS_CONTROL_BASE_ADDR + RCGCADC)      = 0x1;      // Provide system clock to ADC0
     *(uint32_t *)(SYS_CONTROL_BASE_ADDR + RCGCGPIO)     = 0x10;     // Provide system clock to Port E
     asm(" NOP");
     *(uint32_t *)(GPIO_PORTEP_BASE_ADDR + GPIODEN)      = 0x0;      // Disable digital functions
+    *(uint32_t *)(GPIO_PORTEP_BASE_ADDR + GPIOAFSEL)    = 0x4;      // Select alternate function for PE2
     *(uint32_t *)(GPIO_PORTEP_BASE_ADDR + GPIOAMSEL)    = 0x4;      // Enable analog function of PE2
 
-    *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCACTSS)    &= (~0x2);   // Disable Sample Sequencer 1
-    *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCEMUX)     &= (~0xF0);  // Select processor as trigger (via ADCPSSI)
-    *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCSSMUX1)    = 0x1;      // Use AIN1 as input sample
-    *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCSSCTL1)    = 0x2;      // 1st sample is the last in the sequence
-    *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCACTSS)    |= 0x2;      // Enable Sample Sequencer 1
+    *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCACTSS)     = 0x0;      // Disable Sample Sequencer 3
+    *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCEMUX)      = 0x0;      // Select processor as trigger (via ADCPSSI)
+    *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCSSMUX3)    = 0x1;      // Use AIN1 as input sample
+    *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCSSCTL3)    = 0x6;      // 1st sample is the last in the sequence
+    *(uint32_t *)(ANALOG_DIG0_BASE_ADDR + ADCACTSS)     = 0x8;      // Enable Sample Sequencer 3
 }
-
-/*
-\
-
-    7. Enable the sample sequencer logic by setting the corresponding ASENn bit in the ADCACTSS
-        register
- */
