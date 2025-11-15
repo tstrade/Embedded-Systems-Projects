9ba5c04c6efb6caab54d896ef3d0f73d0123815c
Thomas <tstrade@buffalo.edu>
2025-11-15 00:29:18 -0500

    feat: Added all command definitions

--- Tiva_C-Series_HC-05_Bluetooth_SPP/HC05_Commands.c
+++ Tiva_C-Series_HC-05_Bluetooth_SPP/HC05_Commands.c
@@ -57,7 +57,66 @@ cmd test = { 0, 1, "AT", "OK" };
 cmd reset = { 0, 1, "AT+RESET", "OK" };
 cmd firmware_version = { 0, 2, "AT+VERSION?", "+VERSION:" };
 cmd restore_default = { 0, 1, "AT+ORGL", "OK" };
+
 cmd get_module_addr = { 0, 2, "AT+ADDR?", "+ADDR:" };
+
 cmd set_module_name = { 1, 1, "AT+NAME=", "OK" };
-cmd get_module_name = { 0, 2, "AT+NAME?", "+NAME:" };
+cmd check_module_name = { 0, 2, "AT+NAME?", "+NAME:" };
+
+cmd get_btdev_name = { 1, 2, "AT+RNAME?", "+NAME:" };
+
+cmd set_module_mode = { 1, 1, "AT+ROLE=", "OK" };
+cmd check_module_mode = { 0, 1, "AT+ROLE?", "+ROLE:" };
+
+cmd set_device_class = { 1, 1, "AT+CLASS=", "OK" };
+cmd check_device_class = { 0, 2, "AT+CLASS?", "+CLASS:" };
+
+cmd set_giac = { 1, 1, "AT+IAC=", "OK" };
+cmd check_giac = { 0, 2, "AT+IAC?", "+IAC:" };
+
+cmd set_query_patterns = { 3, 1, "AT_INQM=", "OK" };
+cmd check_query_patterns = { 0, 2, "AT+INQM?", "+INQM:" };
+
+cmd set_pin_code = { 1, 1, "AT+PSWD=", "OK" };
+cmd check_pin_code = { 0, 2, "AT+PSWD", "+PSWD:"}
+
+cmd set_serial_param = { 3, 1, "AT+UART=", "OK" };
+cmd check_serial_param = { 0, 2, "AT+UART?", "+UART=" };
+
+cmd set_connect_mode = { 1, 1, "AT+CMDOE=", "OK" };
+cmd check_connect_mode = { 0, 2, "AT+CMODE?", "+CMODE:" };
+
+cmd set_fixed_addr = { 1, 1, "AT+BIND=", "OK" };
+cmd check_fixed_addr = { 0, 2, "AT+BIND?", "+BIND:" };
+
+cmd set_led_io = { 2, 1, "AT+POLAR=", "OK" };
+cmd check_led_io = { 0, 2, "AT+POLAR?", "+POLAR:" };
+
+cmd set_pio_output = { 2, 1, "AT+PIO=", "OK" };
+
+cmd set_scan_param = { 4, 1, "AT+IPSCAN=", "OK" };
+cmd check_scan_param = { 0, 2, "AT+IPSCAN?", "+IPSCAN:" };
+
+cmd set_sniff_param = { 4, 1, "AT+SNIFF=", "OK" };
+cmd check_sniff_param = { 0, 2, "AT+SNIFF?", "+SNIFF:" };
+
+cmd set_security_mode = { 2, 1, "AT+SENM=", "OK" };
+cmd check_security_mode = { 0, 2, "AT+SENM?", "+SENM:" };
+
+cmd delete_auth_dev = { 1, 1, "AT+PMSAD", "OK" };
+cmd delete_all_auth_dev = { 0, 1, "AT_RMAAD", "OK" };
+cmd search_auth_dev = { 1, 2, "AT+FSAD?", "OK" };
+cmd get_auth_dev_count = { 0, 2, "AT+ADCN?", "+ADCN:" };
+cmd mru_auth_dev = { 0, 2, "AT+MRAD?", "+MRAD:" };
+
+cmd get_module_working_state = { 0, 2, "AT+STATE?", "+STATE:" };
+init_spp_profile_lib = { 0, 1, "AT+INIT", "OK" };
+
+// cmd inquiry_btdev = { 0, } <- this one is weird. not clear how many responses to expect
+cmd cancel_inquiry_btdev = { 0, 1, "AT+INQC", "OK" };
+
+cmd equipment_matching = { 2, 1, "AT+PAIR=", "OK" };
+
+cmd connect_device = { 1, 1, "AT+LINK=", "OK" };
+cmd disconnect = { 1, 2, "AT+DISC=", "+DISK:" };
 
--- Tiva_C-Series_HC-05_Bluetooth_SPP/HC05_Commands.h
+++ Tiva_C-Series_HC-05_Bluetooth_SPP/HC05_Commands.h
@@ -1,8 +1,6 @@
 #ifndef __HC05_COMMANDS_H__
 #define __HC05_COMMANDS_H__
 
-#pragma once
-
 #include <inttypes.h>
 #include <stdarg.h>
 
@@ -12,58 +10,6 @@
 
 */
 
-/*
-Struct comms =
-{
-    { , "OK" },
-    { "AT_NAME=", "OK" },
-    { "AT+NAME?", "+NAME:", "OK", "FAIL" },
-    { "AT+ROLE=", "OK" },
-    { "AT+ROLE?", "+ROLE:", "OK" },
-    { "AT+STATE?", "+STATE:", "OK" },
-
-    { "AT+RNAME?", "+NAME:", "OK", "FAIL" },
-    { "AT+CLASS=", "OK" },
-    { "AT+CLASS?", "+CLASS:", "OK", "FAIL" },
-    { "AT+INQ", "+INQ:", "OK" },
-    { "AT+INQC", "OK" },
-    { "AT+PAIR=", "OK", "FAIL" },
-    { "AT+DISC", "+DISC:SUCCESS", "+DISC:LINK_LOSS", "+DISC:NO_SLC", "+DISC:TIMEOUT", "+DISC:ERROR", "OK" },
-
-    { "AT+PMSAD=", "OK" },
-    { "AT+RMAAD", "OK" },
-    { "AT+FSAD=", "OK", "FAIL" },
-    { "AT+ADCN?", "+ADCN:", "OK" },
-    { "AT+MRAD?", "+MRAD:", "OK" },
-
-    { "AT+IAC=", "OK", "FAIL" },
-    { "AT+IAC", "+IAC:", "OK" },
-    { "AT+INQM=", "OK", "FAIL" },
-    { "AT+INQM?", "+INQM:", "OK" },
-    { "AT+PSWD=", "OK" },
-    { "AT+PSWD?", "+PSWD:", "OK" },
-    { "AT+UART=", "OK" },
-    { "AT+UART?", "+UART=", "OK" }
-    { "AT+CMODE=", "OK" }
-    { "AT+CMODE?", "+CMODE:", "OK" },
-    { "AT+BIND=", "OK" },
-    { "AT+BIND?", "+BIND:", "OK" },
-    { "AT+POLAR=", "OK" },
-    { "AT+POLAR?", "+POLAR=", "OK" },
-    { "AT+PIO=", "OK" },
-    { "AT+IPSCAN=", "OK" },
-    { "AT+IPSCAN?", "+IPSCAN:", "OK" },
-    { "AT+SNIFF=", "OK" },
-    { "AT+SNIFF?", "+SNIFF:", "OK" },
-    { "AT+SENM=", "OK", "FAIL" },
-    { "AT+SENM?", "+SENM:", "OK" },
-
-    { "AT+INIT", "OK", "FAIL" },
-    { "AT+PAIR=", "OK", "FAIL" },
-    { "AT+ENSNIFF=", "OK" },
-    { "AT+EXSNIFF=", "OK" }
-}
-*/
 
 typedef struct cmd
 {
@@ -77,89 +23,68 @@ extern cmd test;
 extern cmd reset;
 extern cmd firmware_version;
 extern cmd restore_default;
+
 extern cmd get_module_addr;
+
 extern cmd set_module_name;
-extern cmd get_module_name;
+extern cmd check_module_name;
 
+extern cmd get_btdev_name;
 
+extern cmd set_module_mode;
+extern cmd check_module_mode;
 
+extern cmd set_device_class;
+extern cmd check_device_class;
 
-/*
-cmd set_module_mode[2]; // Param: 0= Slave, 1= Master, 2= Slave-Loop
-cmd get_module_mode[3];
-cmd get_module_state[3]; // INITIALIZED, READY, PAIRABLE, PAIRED, INQUIRING, CONNECTING, DISCONNECTED, NUKNOW
+extern cmd set_giac;
+extern cmd check_giac;
 
- Device Info
-cmd get_btdev_name[4]; // Param: Address of Bluetooth device
-cmd set_btdev_class[2]; // Param: Device Class
-cmd get_btdev_class[4];
-cmd inquiry_btdev[3];
-cmd cancel_inquiry_btdev[2];
-cmd connect_device[3]; // Param: Device addr
-cmd disconnect_btdev[7]; // SUCCESS, LINK_LOSS, NO_SLC, TIMEOUT, ERROR
+extern cmd set_query_patterns;
+extern cmd check_query_patterns;
 
- Authenticated Devices
-cmd delete_auth_dev[2]; // Param: Authenticated Device addr
-cmd delete_all_auth_dev[2];
-cmd search_auth_dev[3]; // Param: Device addr
-cmd get_auth_dev_count[3];
-cmd mru_auth_dev[3];
+extern cmd set_pin_code;
+extern cmd check_pin_code;
 
- General Inquire Access Code
-cmd set_giac[3]; // Param: GIAC (Default: 9e8b33)
-cmd get_giac[3];
+extern cmd set_serial_param;
+extern cmd check_serial_param;
 
- Configurations
-cmd set_query_access_pattern[3]; // Param 1: 0= standard, 1= rssi
-                                            // Param 2: Max # of BT devices to respond to
-                                            // Param 3: Timeout (1-48 : 1.28s - 61.44s)
-cmd get_query_access_pattern[3];
+extern cmd set_connect_mode;
+extern cmd check_connect_mode;
 
-cmd set_pin_code[2]; // Param: PIN code (Default 1234)
-cmd get_pin_code[3];
+extern cmd set_fixed_addr;
+extern cmd check_fixed_addr;
 
-cmd set_serial_parameter[2]; // Param 1: Baud
-                                        // Param 2: Stop bit
-                                        // Param 3: Parity
-cmd get_serial_parameter[3];
+extern cmd set_led_io;
+extern cmd check_led_io;
 
-cmd set_connect_mode[2]; // Param: 0= connect fixed addr, 1= connect any addr, 2= slave-loop
-cmd get_connect_mode[3];
+extern cmd set_pio_output;
 
-cmd set_fixed_addr[2]; // Param: Fixed address (Default 00:00:00:00:00:00)
-cmd get_fixed_addr[3];
+extern cmd set_scan_param;
+extern cmd check_scan_param;
 
-cmd set_led_io[2]; // Param 1: 0= PIO8 low drive LED, 1= PIO8 high drive LED
-                            // Param 2: 0= PIO9 low drive LED, 1= PIO9 high drive LED
-cmd get_led_io[3];
+extern cmd set_sniff_param;
+extern cmd check_sniff_param;
 
-cmd set_pio_output[2]; // Param 1: PIO number
-                                // Param 2: PIO level (0= low, 1= high)
+extern cmd set_security_mode;
+extern cmd check_security_mode;
 
-cmd set_scan_parameter[2]; // Param 1: Query time interval
-                                    // Param 2: Query duration
-                                    // Param 3: Paging interval
-                                    // Param 4: Call duration
-cmd get_scan_parameter[3];
+extern cmd delete_auth_dev;
+extern cmd delete_all_auth_dev;
+extern cmd search_auth_dev;
+extern cmd get_auth_dev_count;
+extern cmd mru_auth_dev;
 
-cmd set_shiff_parameter[2]; // Param 1: Max time
-                                    // Param 2: Min time
-                                    // Param 3: Retry time
-                                    // Param 4: Time out
-cmd get_shiff_paramter[3];
+extern cmd get_module_working_state;
+extern init_spp_profile_lib;
 
-cmd set_security_mode[3]; // Param 1 (Sec mode): 0= off, 1= non-secure, 2= service, 3= link, 4= mode unknown
-                                    // Param 2 (Hci mode): 0= off, 1= pt to pt, 2= pt to pt and bcast
-cmd get_security_mode[3];
+extern cmd inquiry_btdev;
+extern cmd cancel_inquiry_btdev;
 
+extern cmd equipment_matching;
 
-cmd init_spp_profile[3];
-cmd equipment_matching[3]; // Param 1: Device addr
-                                    // Param 2: Time out
-
-cmd energy_saving_mode[2]; // Param: Device addr
-cmd exerts_energy_saving_mode[2]; // Param: Device addr
-*/
+extern cmd connect_device;
+extern cmd disconnect;
 
 
 void send_command ( cmd, ... );
